9fd2572dee1f042668964d5763d52dcb27fb6efe
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-31 15:21:39 +0700

    promise pass ONLY ONE parameter to resolve(<res>), solve problem by global-variable

--- index.js
+++ index.js
@@ -14,62 +14,64 @@ let sheets = google.sheets('v4');
  * Step 1: Open 'lunchMoneyId', get out users
  * @type {[type]}
  */
+let globalAuth;
+let globalUsers;
 
 oauth2Promise
-	.then(auth => {
-	  // console.log(auth);
-	  console.log('\033[32mAuth\033[0m: success');
-	  console.log(auth);
-	  
-	  let getUsersPromise = new Promise((resolve, reject) => {
-	  	sheets.spreadsheets.values.get({
-		    auth: auth,
-		    spreadsheetId: lunchMoneyId,
-		    range: '2016!1:1'
-		  }, (err, res) => {
-		    if (err) {
-		      reject('The API returned an error: ' + err);
-		    }
+  .then(auth => {
+    // console.log(auth);
+    console.log('\033[32mAuth\033[0m: success');
+    console.log(auth);
+    globalAuth = auth;
 
-		    resolve(res, auth);
-		  });
-	  });
+    let getUsersPromise = new Promise((resolve, reject) => {
+      sheets.spreadsheets.values.get({
+        auth: globalAuth,
+        spreadsheetId: lunchMoneyId,
+        range: '2016!1:1'
+      }, (err, res) => {
+        if (err) {
+          reject('The API returned an error: ' + err);
+        }
 
-	  return getUsersPromise;
-	})
-	.then((res, auth) => {
-		// console.log(res);
-		console.log('\033[32mGot users\033[0m: success');
-		console.log(res);
-		console.log(auth);
+        resolve(res);
+      });
+    });
 
-		let rows = res.values;
-		if(!rows || rows.length == 0){
-			reject('Fail to users from getUsersPromise_res');
-		}
-		//["", "Hoan", "Loc", "Duy", "Hoang", "Vinh", "Toan web", "Nam", "D. Vu"]
-		let users = rows[0];
-		// console.log(users);
+    return getUsersPromise;
+  })
+  .then(res => {
+    // console.log(res);
+    console.log('\033[32mGot users\033[0m: success');
+    console.log(res);
 
-		let getMenuInWeek = new Promise((resolve, reject)=>{
-			sheets.spreadsheets.values.get({
-		    auth: auth,
-		    spreadsheetId: nuiBayUtItId,
-		    range: 'Gia chanh Cam Tuyet!A558:AD581'
-		  }, (err, res2) => {
-		    if (err) {
-		      reject('The API returned an error: ' + err);
-		    }
+    let rows = res.values;
+    if (!rows || rows.length == 0) {
+      reject('Fail to users from getUsersPromise_res');
+    }
+    //["", "Hoan", "Loc", "Duy", "Hoang", "Vinh", "Toan web", "Nam", "D. Vu"]
+    globalUsers = rows[0];
+    // console.log(users);
 
-		    resolve(users, res2);
-		  });
-		});
+    let getMenuInWeek = new Promise((resolve, reject) => {
+      sheets.spreadsheets.values.get({
+        auth: globalAuth,
+        spreadsheetId: nuiBayUtItId,
+        range: 'Gia chanh Cam Tuyet!A558:AD581'
+      }, (err, res) => {
+        if (err) {
+          reject('The API returned an error: ' + err);
+        }
 
+        resolve(res);
+      });
+    });
 
-
-		return getMenuInWeek;
-	})
-	.then((users, res2)=>{
-		console.log('\033[32mGot menu\033[0m: success');
-		console.log(res2);
-	});
+    return getMenuInWeek;
+  })
+  .then(res => {
+    console.log('\033[32mGot menu\033[0m: success');
+    // console.log(res);
+    console.log(res.values[0]);
+    // console.log(globalUsers);
+  });
