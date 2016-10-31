0964d4258f9e5ec93dd878325559bcb1022306b7
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-31 13:58:22 +0700

    success append to Lunch-Money, Oops

--- sheet.js
+++ sheet.js
@@ -8,7 +8,8 @@ var updateToLunchMoney = require(`${__dirname}/updateToLunchMoney`);
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
--- updateToLunchMoney.js
+++ updateToLunchMoney.js
@@ -1,31 +1,23 @@
 const lunchMoneyId = '1CM6BNJn4K24JZbn5zJLwkcES9BZ4o9wzr8t9W6kNb-8';
 
 function updateToLunchMoney(sheets, auth, record){
-	sheets.spreadsheets.values.get({
+	record = ['hoang', 'anh', 'le'];
+	sheets.spreadsheets.values.append({
 	    auth: auth,
 	    spreadsheetId: lunchMoneyId,
-	    range: '2016!A276:AT',
+	    range: '2016!A277:AT',
+	    valueInputOption: 'USER_ENTERED',
+	    resource: {
+	    	values: [record],
+	    	range: '2016!A277:AT',
+	    	majorDimension: 'ROWS'
+	    }
 	  }, function(err, response) {
 	    if (err) {
 	      console.log('The API returned an error: ' + err);
 	      return;
 	    }
-	    // console.log(response);
-	    let rows = response.values;
-	    // console.log(rows, rows.length);
-	    if(!rows || rows.length == 0) {
-	    	console.log('No data found.');
-	    }else{
-	    	// console.log(rows[0]);
-	    	console.log('read first 10:');
-	    	let firstTen = rows[0]
-		    	.filter((val, index) => {
-		    		return index < 10;
-		    	})
-	    		.join(',');
-    		
-    		console.log(firstTen);
-	    }
+	    console.log(response);
 	  });
 }
 
