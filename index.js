1c414ba1d12812365bdd8cf5eb3b3efa208eed66
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-31 15:13:32 +0700

    build step with promise, BUT how pass auth from promise1>promise2...

--- index.js
+++ index.js
@@ -15,18 +15,61 @@ let sheets = google.sheets('v4');
  * @type {[type]}
  */
 
-oauth2Promise.then(auth => {
-  console.log(auth);
-  sheets.spreadsheets.values.get({
-    auth: auth,
-    spreadsheetId: lunchMoneyId,
-    range: '2016!1:1'
-  }, (err, res) => {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
+oauth2Promise
+	.then(auth => {
+	  // console.log(auth);
+	  console.log('\033[32mAuth\033[0m: success');
+	  console.log(auth);
+	  
+	  let getUsersPromise = new Promise((resolve, reject) => {
+	  	sheets.spreadsheets.values.get({
+		    auth: auth,
+		    spreadsheetId: lunchMoneyId,
+		    range: '2016!1:1'
+		  }, (err, res) => {
+		    if (err) {
+		      reject('The API returned an error: ' + err);
+		    }
 
-    console.log(res)
-  });
-})
+		    resolve(res, auth);
+		  });
+	  });
+
+	  return getUsersPromise;
+	})
+	.then((res, auth) => {
+		// console.log(res);
+		console.log('\033[32mGot users\033[0m: success');
+		console.log(res);
+		console.log(auth);
+
+		let rows = res.values;
+		if(!rows || rows.length == 0){
+			reject('Fail to users from getUsersPromise_res');
+		}
+		//["", "Hoan", "Loc", "Duy", "Hoang", "Vinh", "Toan web", "Nam", "D. Vu"]
+		let users = rows[0];
+		// console.log(users);
+
+		let getMenuInWeek = new Promise((resolve, reject)=>{
+			sheets.spreadsheets.values.get({
+		    auth: auth,
+		    spreadsheetId: nuiBayUtItId,
+		    range: 'Gia chanh Cam Tuyet!A558:AD581'
+		  }, (err, res2) => {
+		    if (err) {
+		      reject('The API returned an error: ' + err);
+		    }
+
+		    resolve(users, res2);
+		  });
+		});
+
+
+
+		return getMenuInWeek;
+	})
+	.then((users, res2)=>{
+		console.log('\033[32mGot menu\033[0m: success');
+		console.log(res2);
+	});
