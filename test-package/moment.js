835c5db48de9b7a991e6e70230c6c9103b97906d
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-11-07 09:36:37 +0700

    utcOffset helps change timezone, moment

--- test-package/moment.js
+++ test-package/moment.js
@@ -1,3 +1,5 @@
 let moment = require('moment');
 let repl = require('repl');
+let todayInVietnamTimezone = moment().utcOffset(7 * 60).format();
+console.log('todayInVietnamTimezone', todayInVietnamTimezone);
 repl.start('>').context.moment = moment;
