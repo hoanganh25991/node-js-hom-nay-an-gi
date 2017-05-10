0c47fdc028d99b4227723e5f18f08e7911bcb91d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-10 15:15:09 +0700

    Update auth fail case, ask admin for auto renew token

--- trace.config.js
+++
@@ -1,5 +0,0 @@
-// trace.config.js 
-module.exports = {
-   serviceName: 'node-js-hom-nay-an-gi',
-   apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4Zjk3NDhjNTgzYmIxMzI4YTc5Y2JiNSIsImlhdCI6MTQ5Mjc0MzMxMX0.frXYekfwe2Np8RPB3iKo9g59SuCMvtTr7Ej3_o1n3Ig'
-};
